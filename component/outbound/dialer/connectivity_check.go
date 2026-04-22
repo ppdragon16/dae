97a1e327526074228a28857f0a6d2bb14dacecd8
ppdragon16 <>
2026-04-22 13:31:54 +0000

    Fix a bug where fixed(0) policy doesn't have timeout penalty.

--- component/outbound/dialer/connectivity_check.go
+++ component/outbound/dialer/connectivity_check.go
@@ -437,7 +437,7 @@ func (d *Dialer) Update(ok bool, latency time.Duration, networkType *common.Netw
 				maxTimeoutPenalty = p
 			}
 		}
-		if latency > maxTimeoutPenalty {
+		if latency > maxTimeoutPenalty && maxTimeoutPenalty > 0 {
 			ok = false
 		}
 	}
@@ -445,7 +445,10 @@ func (d *Dialer) Update(ok bool, latency time.Duration, networkType *common.Netw
 	d.alive = ok
 	for g := range d.registeredDialerGroups {
 		if !ok {
-			latency = g.GetTimeoutPenalty()
+			penalty := g.GetTimeoutPenalty()
+			if penalty > 0 {
+				latency = penalty
+			}
 		}
 		alpha := g.GetEmaAlpha()
 		if d.MovingAverage[g] == 0 {
